ceface5dacad9a212beec417c27967a8fad94e80
Gerald <gprmarquez@gmail.com>
2022-09-07 22:15:55 -0500

    adding dependencies for navigation, assets

--- App.tsx
+++ App.tsx
@@ -4,7 +4,36 @@ import { SafeAreaProvider } from 'react-native-safe-area-context';
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import React from 'react';
+import { View, Text } from 'react-native';
 
+function HomeScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Home Screen</Text>
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
+
+// export default function App() {
+//   const isLoadingComplete = useCachedResources();
+//   const colorScheme = useColorScheme();
+
+//   if (!isLoadingComplete) {
+//     return null;
+//   } else {
+//     return (
+//       <SafeAreaProvider>
+//         <Navigation colorScheme={colorScheme} />
+//         <StatusBar />
+//       </SafeAreaProvider>
+//     );
+//   }
+// }
 export default function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
@@ -13,10 +42,11 @@ export default function App() {
     return null;
   } else {
     return (
-      <SafeAreaProvider>
-        <Navigation colorScheme={colorScheme} />
-        <StatusBar />
-      </SafeAreaProvider>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name='Home' component={HomeScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
     );
   }
 }
---
+++ assets/colors/colors.js
@@ -0,0 +1,11 @@
+const colors = {
+    blue: '#0E4686',
+    green: '#1A746B',
+    red: '#BD1C39',
+    lightGreen: '#7CCD84',
+    orange: '#F2A418',
+    lightBlue: '#3392FF',
+    black: '#030E1B',
+};
+
+export default colors;
