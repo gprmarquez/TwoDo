4de7922146ee3995aae30dcf487f9b792a6b3118
Gerald <gprmarquez@gmail.com>
2022-11-08 01:39:59 -0600

    moving files for clarity and learning

--- screens/TabTwoScreen.tsx
+++ screens/TabTwoScreen.tsx
@@ -1,7 +1,7 @@
 import { StyleSheet } from "react-native";
 
-import EditScreenInfo from "../components/EditScreenInfo";
-import { Text, View } from "../components/Themed";
+import EditScreenInfo from "../components/fromReactNative/EditScreenInfo";
+import { Text, View } from "../components/fromReactNative/Themed";
 
 export default function TabTwoScreen() {
   return (
